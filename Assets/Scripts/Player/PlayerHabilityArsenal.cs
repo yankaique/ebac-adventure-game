6185e9ed3861a8248ef7f61e3b9d4d9652eb4860
yankaique <yan.contato.dev@gmail.com>
2023-10-14 17:15:58 -0300

    feat: create new script to switch guns

--- Assets/Scripts/Player/PlayerHabilityArsenal.cs
+++ Assets/Scripts/Player/PlayerHabilityArsenal.cs
@@ -2,27 +2,50 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 
-public class PlayerHabilityShoot : PlayerHabilityBase
+public class PlayerHabilityArsenal : PlayerHabilityBase
 {
     public List<UIGunUpdater> uiGunUpdaters;
+    public TMP_Text gunUIText;
 
-    public GunBase gunBase;
+    [Header("First Gun Props")]
+    public GunBase firstGunBase;
+    public string firstGunName = "First Gun";
+
+    [Header("Second Gun Props")]
+    public GunBase secondGunBase;
+    public string secondGunName = "Second Gun";
+
     public Transform gunPosition;
     private GunBase _currentGun;
+
     protected override void Init()
     {
         base.Init();
-        CreateGun();
+        CreateGun(firstGunBase, firstGunName);
 
         inputs.Gameplay.Shoot.performed += ctx => StartShoot();
         inputs.Gameplay.Shoot.canceled += ctx => CancellShoot();
     }
 
-    private void CreateGun()
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Keypad1))
+        {
+            CreateGun(firstGunBase, firstGunName);
+        }
+        else if(Input.GetKeyDown(KeyCode.Keypad2))
+        {
+            CreateGun(secondGunBase, secondGunName);
+        }
+    }
+
+    private void CreateGun(GunBase gunBase, string gunName = "")
     {
         _currentGun = Instantiate(gunBase, gunPosition);
         _currentGun.transform.localPosition = _currentGun.transform.localEulerAngles = Vector3.zero;
+        gunUIText.text = gunName;
     }
     
     private void StartShoot()
