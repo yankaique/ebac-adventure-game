a8b46a847f60da6a97c724ff41dedafa24c13667
yankaique <yan.contato.dev@gmail.com>
2023-10-28 08:54:14 -0300

    feat: add post process and camera perlin

--- Assets/Scripts/Utils/ShakeCamera.cs
+++ Assets/Scripts/Utils/ShakeCamera.cs
@@ -4,32 +4,36 @@ using UnityEngine;
 using Ebac.core.Singleton;
 using Cinemachine;
 
-public class ShakeCamera : Singleton<ShakeCamera>
+namespace Camera
 {
-    public CinemachineVirtualCamera virtualCamera;
 
-    public float shakeTime;
-
-    public void Shake(float amplitude, float frequency, float time)
+    public class ShakeCamera : Singleton<ShakeCamera>
     {
-        var channel = virtualCamera.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
-        channel.m_AmplitudeGain = amplitude;
-        channel.m_FrequencyGain = frequency;
+        public CinemachineVirtualCamera virtualCamera;
 
-        shakeTime = time;
-    }
+        public float shakeTime;
 
-    private void Update()
-    {
-        if(shakeTime > 0)
-        {
-            shakeTime -= Time.deltaTime;
-        }
-        else
+        public void Shake(float amplitude, float frequency, float time)
         {
             var channel = virtualCamera.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
-            channel.m_AmplitudeGain = 0f;
-            channel.m_FrequencyGain = 0f;
+            channel.m_AmplitudeGain = amplitude;
+            channel.m_FrequencyGain = frequency;
+
+            shakeTime = time;
+        }
+
+        private void Update()
+        {
+            if(shakeTime > 0)
+            {
+                shakeTime -= Time.deltaTime;
+            }
+            else
+            {
+                var channel = virtualCamera.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
+                channel.m_AmplitudeGain = 0f;
+                channel.m_FrequencyGain = 0f;
+            }
         }
     }
 }
