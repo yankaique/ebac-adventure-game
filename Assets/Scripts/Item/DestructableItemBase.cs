92cb1850ed55b5bfe29388201f6e416abd4a28a5
yankaique <yan.contato.dev@gmail.com>
2023-10-29 19:59:39 -0300

    feat: droop coins with object

--- Assets/Scripts/Item/DestructableItemBase.cs
+++ Assets/Scripts/Item/DestructableItemBase.cs
@@ -1,4 +1,5 @@
 using DG.Tweening;
+using System.Collections;
 using UnityEngine;
 
 public class DestructableItemBase : MonoBehaviour
@@ -6,6 +7,12 @@ public class DestructableItemBase : MonoBehaviour
     public HealthBase healthBase;
 
     public float shakeDuration = 1.0f;
+
+    [Header("Coin Drop")]
+    public GameObject coinPrefab;
+    public Transform dropPosition;
+    public int dropCoinsAmount = 10;
+
     public int shakeForce = 1;
 
     private void OnValidate()
@@ -22,6 +29,28 @@ public class DestructableItemBase : MonoBehaviour
     private void OnDamage(HealthBase h)
     {
         transform.DOShakeScale(shakeDuration, Vector3.up, shakeForce);
+        DropGroupOfCoins();
+    }
+
+    private void DropCoins()
+    {
+        var i = Instantiate(coinPrefab);
+        i.transform.position = dropPosition.position;
+        i.transform.DOScale(0, 1f).SetEase(Ease.OutBack).From();
+    }
+
+    private void DropGroupOfCoins()
+    {
+        StartCoroutine(DropGroupOfCoinsCoroutine());
+    }
+
+    IEnumerator DropGroupOfCoinsCoroutine()
+    {
+        for (int i = 0; i < dropCoinsAmount; i++)
+        {
+            DropCoins();
+            yield return new WaitForSeconds(.1f);
+        }
     }
 
 }
